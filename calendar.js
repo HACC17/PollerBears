c54b98fecb24fff5825b4b8212d9c732e0f4df1e
sgalutira <sheena.galutira@gmail.com>
2017-09-03 18:51:01 -1000

    installed react framework

--- calendar.js
+++ calendar.js
@@ -1,8 +1,8 @@
 // Booking.js still needs access to jquery, so make sure its available
-require('jquery')
+require('jquery');
 // Pull in the module, ES2015 imports also works:
 // import TimekitBooking from 'timekit-booking'
-var TimekitBooking = require('timekit-booking')
+var TimekitBooking = require('timekit-booking');
 // Booking.js is now available as local var TimekitBooking instead of global window.timekitBooking
 var widget = new TimekitBooking();
 console.log(widget);
