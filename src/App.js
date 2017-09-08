582bf998be724eb82da10747c7c504907af94891
sgalutira <sheena.galutira@gmail.com>
2017-09-07 19:44:51 -1000

    added react router for links

--- src/App.js
+++ src/App.js
@@ -1,18 +1,49 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Home from './containers/Home';
-import logo from './logo.svg';
-import './App.css';
+
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state={
+      view: true
+    };
+    this.handleView = this.handleView.bind(this);
+  }
+
+  handleView(){
+    this.setState({view: true});
+  }
+
   render() {
     return (
-      <div className="App">
-        <div class="nav">
-          <button>Locations</button>
-          <button>Contact</button>
-          <button>FAQs</button>
-        </div>
-          <Home />
+      <div className="App-container">
+        <Router>
+          <div className="nav-header">
+            <div>
+              <h1><Link to="/">VOTENTEER</Link></h1>
+              <ul>
+                <li>
+                  <Link to="/locations">Locations</Link>
+                </li>
+                <li>
+                  <Link to="/Contact">Contact</Link>
+                </li>
+                <li>
+                  <Link to="/faqs">FAQs</Link>
+                </li>
+                <li>
+                  <Link to="/register">register</Link>
+                </li>
+              </ul>
+
+            </div>
+              <div className="content-container">
+                <Route exact path="/" component={Home}/>
+              </div>
+          </div>
+        </Router>
       </div>
     );
   }
