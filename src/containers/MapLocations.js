b1f0823522661644382c4589b68182353a20cefe
sgalutira <sheena.galutira@gmail.com>
2017-09-08 22:46:30 -1000

    radio buttons for sign up with state change

--- src/containers/MapLocations.js
+++ src/containers/MapLocations.js
@@ -2,6 +2,24 @@ import React, { Component } from 'react';
 import Breadcrumbs from '../components/Breadcrumbs';
 
 class MapLocations extends Component {
+	constructor(props){
+		super(props);
+		this.state={
+			selectedTraining: 'option1'	
+		};
+		this.handleOptionChange = this.handleOptionChange.bind(this);
+		this.handleFormSubmit = this.handleFormSubmit.bind(this);
+	}
+
+	handleOptionChange (e){
+		this.setState({selectedTraining: e.target.value});
+	}
+
+	handleFormSubmit(submit){
+		submit.preventDefault();
+		console.log('You signed up for', this.state.selectedTraining);
+	}
+
 	render(){
 		return(
 			<div>
@@ -20,7 +38,38 @@ class MapLocations extends Component {
 						Address:
 						Times:
 					</div>
-					<button>Sign Up</button>
+					<div className="signup-form">
+						<form onSubmit={this.handleFormSubmit}>
+							<div className="radio">
+								<label>
+									<input type="radio" value="option1" 
+										checked={this.state.selectedTraining==='option1'}
+										onChange={this.handleOptionChange}
+										/>
+									Option 1
+								</label>
+							</div>
+							<div className="radio">
+								<label>
+									<input type="radio" value="option2" 
+										checked={this.state.selectedTraining==='option2'}
+										onChange={this.handleOptionChange}
+										/>
+									Option 2
+								</label>
+							</div>
+							<div className="radio">
+								<label>
+									<input type="radio" value="option3" 
+									checked={this.state.selectedTraining==='option3'}
+									onChange={this.handleOptionChange}
+									/>
+									Option 3
+								</label>
+							</div>
+							<button type="submit">Sign Up</button>
+						</form>
+					</div>
 				</div>
 			</div>
 		);
