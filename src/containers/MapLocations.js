d7ffce21d3dd90416fa4ee2ea430c2dac6581ff4
sgalutira <sheena.galutira@gmail.com>
2017-09-08 22:21:10 -1000

    added button

--- src/containers/MapLocations.js
+++ src/containers/MapLocations.js
@@ -11,6 +11,16 @@ class MapLocations extends Component {
 				What part of the island do you want to be trained at?
 				<div className="MapLocations-container">
 					MAP GOES HERE
+					<div>
+						District: <br/>
+						Place
+					</div>
+					<div className="location-list">
+						Name:
+						Address:
+						Times:
+					</div>
+					<button>Sign Up</button>
 				</div>
 			</div>
 		);
