0fb7b394374cdee59be442accd228cdef56ac3ca
ynikki <808nikz@gmail.com>
2017-09-10 00:01:43 -1000

    Adding classname to each position description

--- src/components/PositionDescriptions.js
+++ src/components/PositionDescriptions.js
@@ -4,13 +4,13 @@ class PositionDescription extends Component {
 	render(){
 		return(
 			<div>
-				<div>
-					<b>Controler Center Operator</b>
+				<div className="control-center-operator">
+					<p><strong>Controler Center Operator</strong><br />
 					Responds to telephone inquiries and request from polling places using a computer-based call center system. Duties include entering information, determining solutions, and resolving problems.
-					Stipend: $85
+					Stipend: $85</p>
 				</div>
-				<div>
-					<b>Counting Center Official</b>
+				<div className="counting-center-official">
+					<p><strong>Counting Center Official</strong><br />
 					Processes, tabulates, and disseminates election results in an accurate and timely manner. Consist of nine (9) teams.
 
 						Stipend: $85-95
@@ -41,20 +41,22 @@ class PositionDescription extends Component {
 								Receiving Team: Accepts custody of voted ballots, precinct cans, and voting equipment at the counting center.
 							</li>
 						</ul>
+					</p>
 				</div>
-				<div>
-					<b>Delivery/Collection Official</b>
+				<div className="delivery-collection-official">
+					<p><strong>Delivery/Collection Official</strong><br />
 					Delivers ballots and election supplies to the polling places on election morning. Collects voted ballots and election supplies after the polls have closed on election day. Each team consist of two members.
 
 						Stipend: $50 – $95
-					<ul>
-						<li>
-							Chair: Coordinates and directs the activities of the D/C Team. Completes documentation and certifies the sealing of the election supplies.
-						</li>
-						<li>
-							Member: Assist the Chair and witnesses the transfer of custody of ballots and election supplies
-						</li>
-					</ul>
+						<ul>
+							<li>
+								Chair: Coordinates and directs the activities of the D/C Team. Completes documentation and certifies the sealing of the election supplies.
+							</li>
+							<li>
+								Member: Assist the Chair and witnesses the transfer of custody of ballots and election supplies
+							</li>
+						</ul>
+					</p>
 				</div>
 				<div>
 					<b>Election Information Services Official</b>
@@ -62,14 +64,14 @@ class PositionDescription extends Component {
 
 					Stipend: $85
 				</div>
-				<div>
-					<b>Facility Official</b>
+				<div className="facility-official">
+					<p><strong>Facility Official</strong><br />
 					Sets up election equipment at polling places on election eve. Provides access to polling places on election day. Disassembles and stores the election equipment after the polls close on election day.
 
-					Stipend: $60 – $140
+					Stipend: $60 – $140</p>
 				</div>
-				<div>
-					<b>Precinct Official</b>
+				<div className="precinct-official">
+					<p><strong>Precinct Official</strong><br />
 					Assist voters at the polling place while ensuring the integrity of the voting process. Precinct Officials operate the Information and Ballot Demonstration Station, Poll Book Station, Ballot Box Station, and eSlate Station.
 
 					Stipend: $5 – $175
@@ -89,14 +91,14 @@ class PositionDescription extends Component {
 								Alternate Election Officials: Stands-by at home on election morning for dispatch to a polling place that requires additional staffing.
 							</li>
 						</ul>
+					</p>
 				</div>
-				<div>
-					<b>Precinct Troubleshooter</b>
+				<div className="precinct-troubleshooter">
+					<p><strong>Precinct Troubleshooter</strong>
 					Monitors the polling places to ensure Officials are following standard operating procedures; and replenishes supplies as necessary. Must have a valid driver’s license.
+					</p>
 				</div>
-				<br/>
-				<br/>
-				<div>
+				<div className="factsheet">
 					<a href="http://elections.hawaii.gov/wp-content/uploads/2017/02/FS401PO002-Election-Day-Officials.pdf">View printable Election Day Officals FACTSHEET</a>
 				</div>
 			</div>
