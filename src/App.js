e25017decd6aa4b3e3029a1d9213b92b4667b734
Lisa <misslisazhou@gmail.com>
2017-09-16 12:42:38 -1000

    added footer

--- src/App.js
+++ src/App.js
@@ -7,28 +7,41 @@ class App extends Component {
     return (
       <div className="App-container">
         <Router>
-          <div className="overall-container">
-            <nav className="custom-nav">
-              <div className="container-fluid">
-                <h1 className="text-left navbar-text"><Link to="/">VOTE<span className="the-n">N</span>TEER</Link></h1>
-                <ul className="nav navbar-nav navbar-right">
-                  <li>
-                    <Link to="/">Home</Link>
-                  </li>
-                  <li>
-                    <Link to="/register">Register</Link>
-                  </li>
-                  <li>
-                    <Link to="/contact">Contact</Link>
-                  </li>
-                  <li>
-                    <Link to="/faqs">FAQs</Link>
-                  </li>
-                </ul>
+          <div>
+            <div className="top-container">
+              <nav className="custom-nav">
+                <div className="container-fluid">
+                  <h1 className="text-left navbar-text"><Link to="/">VOTE<span className="the-n">N</span>TEER</Link></h1>
+                  <ul className="nav navbar-nav navbar-right">
+                    <li>
+                      <Link to="/">Home</Link>
+                    </li>
+                    <li>
+                      <Link to="/register">Register</Link>
+                    </li>
+                    <li>
+                      <Link to="/contact">Contact</Link>
+                    </li>
+                    <li>
+                      <Link to="/faqs">FAQs</Link>
+                    </li>
+                  </ul>
+                </div>
+              </nav>
+              <div className="content-container container">
+                <Routes/>
               </div>
-            </nav>
-            <div className="content-container container">
-              <Routes/>
+            </div>
+            <div className="footer-container">
+              <div className="footer-text row">
+                <div className="col-sm-4">
+                  <h3>Election Dates</h3>
+                  <p>Primary Election: August 11, 2018</p>
+                  <p>General Election: November 6, 2018</p>
+                </div>
+                <div className="col-sm-4">.col-sm-4</div>
+                <div className="col-sm-4">.col-sm-4</div>
+              </div>
             </div>
           </div>
         </Router>
