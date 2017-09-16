f0eb87a2446866c2720bf5e41cca842c06046aa8
Lisa <misslisazhou@gmail.com>
2017-09-15 23:32:04 -1000

    add ids to all inputs, adjust all positionsw= with labels

--- src/components/PositionDescriptions.js
+++ src/components/PositionDescriptions.js
@@ -22,74 +22,120 @@ class PositionDescription extends Component {
 		return(
       <div className="positions-grp">
         <fieldset id="volunteer-positions">
+          <h3>Operational Support Positions</h3>
   				<div className="row">
   					<div className="capitol-position radio-btn col-sm-4">
-              <input type="radio" name="volunteer-positions" value="control-center-operator" id="position-1"/>
-              <label htmlFor="position-1">
+              <input type="radio" name="volunteer-positions" value="control-center-operator" id="bts-pos-1" />
+              <label htmlFor="bts-pos-1">
                 Control Center Operator
               </label>
   					</div>
             <div className="capitol-position radio-btn col-sm-4">
-              <input type="radio" name="volunteer-positions" value="absentee-ballot-team" id="position-2"/>
-              <label htmlFor="position-2">
+              <input type="radio" name="volunteer-positions" value="absentee-ballot-team" id="bts-pos-2" />
+              <label htmlFor="bts-pos-2">
                 Absentee Ballot Team Member
               </label>
             </div>
             <div className="capitol-position radio-btn col-sm-4">
-              <input type="radio" name="volunteer-positions" value="ballot-storage-team" /> Ballot Storage Team
+              <input type="radio" name="volunteer-positions" value="ballot-storage-team" id="bts-pos-3" />
+              <label htmlFor="bts-pos-3">
+                Ballot Storage Team
+              </label>
             </div>
           </div>
           <div className="row">
             <div className="capitol-position radio-btn col-sm-4">
-              <input type="radio" name="volunteer-positions" value="computer-operations-team" /> Computer Operations Team Member
+              <input type="radio" name="volunteer-positions" value="computer-operations-team" id="bts-pos-4" />
+              <label htmlFor="bts-pos-4">
+                Computer Operations Team Member
+              </label>
             </div>
             <div className="capitol-position radio-btn col-sm-4">
-              <input type="radio" name="volunteer-positions" value="duplication-team" /> Duplication Team Member
+              <input type="radio" name="volunteer-positions" value="duplication-team" id="bts-pos-5" />
+              <label htmlFor="bts-pos-5">
+                Duplication Team Member
+              </label>
             </div>
             <div className="capitol-position radio-btn col-sm-4">
-              <input type="radio" name="volunteer-positions" value="manual-audit-team" /> Manual Audit Team Member
+              <input type="radio" name="volunteer-positions" value="manual-audit-team" id="bts-pos-6" />
+              <label htmlFor="bts-pos-6">
+                Manual Audit Team Member
+              </label>
             </div>
           </div>
           <div className="row">
             <div className="capitol-position radio-btn col-sm-4">
-              <input type="radio" name="volunteer-positions" value="official-observer-team" /> Official Observer Team Member
+              <input type="radio" name="volunteer-positions" value="official-observer-team" id="bts-pos-7" />
+              <label htmlFor="bts-pos-7">
+                Official Observer Team Member
+              </label>
             </div>
             <div className="capitol-position radio-btn col-sm-4">
-              <input type="radio" name="volunteer-positions" value="poll-book-audit-team" /> Poll Book Audit Team Member
+              <input type="radio" name="volunteer-positions" value="poll-book-audit-team" id="bts-pos-8" />
+              <label htmlFor="bts-pos-8">
+                Poll Book Audit Team Member
+              </label>
             </div>
             <div className="capitol-position radio-btn col-sm-4">
-              <input type="radio" name="volunteer-positions" value="precinct-can-team" /> Precinct Can Team Member
+              <input type="radio" name="volunteer-positions" value="precinct-can-team" id="bts-pos-9" />
+              <label htmlFor="bts-pos-9">
+                Precinct Can Team Member
+              </label>
             </div>
           </div>
           <div className="row">
             <div className="capitol-position radio-btn col-sm-4">
-              <input type="radio" name="volunteer-positions" value="receiving-team" /> Receiving Team Member
+              <input type="radio" name="volunteer-positions" value="receiving-team" id="bts-pos-10" />
+              <label htmlFor="bts-pos-10">
+                Receiving Team Member
+              </label>
             </div>
             <div className="capitol-position radio-btn col-sm-4">
-              <input type="radio" name="volunteer-positions" value="election-information-services-official" /> Election Information Services Official
+              <input type="radio" name="volunteer-positions" value="election-information-services-official" id="bts-pos-11" />
+              <label htmlFor="bts-pos-11">
+                Election Information Services Official
+              </label>
             </div>
           </div>
-          <p>Precinct Positions</p>
+          <h3>Precinct Positions</h3>
           <div className="row">
             <div className="precinct-position radio-btn col-sm-4">
-              <input type="radio" name="volunteer-positions" value="delivery-collection-official" /> Delivery/Collection Official
+              <input type="radio" name="volunteer-positions" value="delivery-collection-official" id="pre-pos-1" />
+              <label htmlFor="pre-pos-1">
+                Delivery/Collection Official
+              </label>
             </div>
             <div className="precinct-position radio-btn col-sm-4">
-              <input type="radio" name="volunteer-positions" value="facility-official" /> Facility Official
+              <input type="radio" name="volunteer-positions" value="facility-official" id="pre-pos-2" />
+              <label htmlFor="pre-pos-2">
+                Facility Official
+              </label>
             </div>
             <div className="precinct-position radio-btn col-sm-4">
-              <input type="radio" name="volunteer-positions" value="precinct-operations-chair" /> Precinct Operations Chair
+              <input type="radio" name="volunteer-positions" value="precinct-operations-chair" id="pre-pos-3" />
+              <label htmlFor="pre-pos-3">
+                Precinct Operations Chair
+              </label>
             </div>
           </div>
           <div className="row">
             <div className="precinct-position radio-btn col-sm-4">
-              <input type="radio" name="volunteer-positions" value="precinct-operations-official" /> Precinct Operations Official
+              <input type="radio" name="volunteer-positions" value="precinct-operations-official" id="pre-pos-4" />
+              <label htmlFor="pre-pos-4">
+                Precinct Operations Official
+              </label>
             </div>
             <div className="precinct-position radio-btn col-sm-4">
-              <input type="radio" name="volunteer-positions" value="precinct-voter-assistance-official" /> Precinct Voter Assistance Official
+              <input type="radio" name="volunteer-positions" value="precinct-voter-assistance-official" id="pre-pos-5" />
+              <label htmlFor="pre-pos-5">
+                Precinct Voter Assistance Official
+              </label>
             </div>
             <div className="precinct-position radio-btn col-sm-4">
-              <input type="radio" name="volunteer-positions" value="precinct-troubleshooter" /> Precinct Troubleshooter
+              <input type="radio" name="volunteer-positions" value="precinct-troubleshooter" id="pre-pos-6" />
+              <label htmlFor="pre-pos-6">
+                Precinct Troubleshooter
+              </label>
             </div>
           </div>
         </fieldset>
