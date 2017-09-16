e89bf67c8de40bfe595e562c974e1db413115c46
Lisa <misslisazhou@gmail.com>
2017-09-15 22:27:48 -1000

    remove unused divs

--- src/containers/Position.js
+++ src/containers/Position.js
@@ -9,13 +9,8 @@ class Position extends Component {
 				<div>
 				</div>
 				<div className="position-container col-lg-12">
-					<h1>Pick a position you want to volunteer for</h1>
-					<div className="position-info">
-						<div className="circle">
-							Position Descriptions
-							<PositionDescriptions/>
-						</div>
-					</div>
+					<h2>Select A Volunteer Position</h2>
+						<PositionDescriptions/>
 				</div>
 			</div>
 		);
